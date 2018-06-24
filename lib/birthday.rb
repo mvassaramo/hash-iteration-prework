e0bf3200165f6f4c87abc044a7ac96d818c54075
Maduri Vassaramo <maduri.vassaramo@hotmail.co.uk>
2018-06-24 17:25:57 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,8 +5,9 @@ birthday_kids = {
 }
 
 def happy_birthday(birthday_kids)
+  
   birthday_kids.each do |key, value| 
     puts "Happy Birthday #{key}! You are now #{value} years old!"
-end
-
+  end
+end 
 
